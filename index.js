a2de2f9d2565c81d785771937083d1739c74af80
PatrickStojek <patryk.stojek7@gmail.com>
2024-03-26 23:50:49 +0100

    Add showcase of typeof functionality

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const basketInteger = 4791
 
 const sum =  basketFloat1 + basketFloat2 + basketInteger
 
+const avg = sum / 3
+
+//checking the the type of variable
+
+let arr = [];
+const arrType = typeof arr;
+console.log(arrType) // output: object
+
+const fn  = function() {
+}
+
+const fnType = typeof fn
+console.log(fnType); /// output: function
+
